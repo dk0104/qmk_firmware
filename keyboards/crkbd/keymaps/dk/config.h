5f9ca86f1c7f41ca27a4a8a9580ea8a2172ef0f1
Denis Keksel <Denis Keksel>
2023-12-30 23:22:50 +0400

    final version

--- keyboards/crkbd/keymaps/dk/config.h
+++ keyboards/crkbd/keymaps/dk/config.h
@@ -31,8 +31,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL_PD2
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 200
-#define QUICK_TAP_TERM 0
+#define TAPPING_TERM 300
+#define QUICK_TAP_TERM 10
 
 #ifdef RGBLIGHT_ENABLE
 #    undef RGBLED_NUM
